dd00e851ffafc0c638cb331680d8a15540e69fa0
Addy Samuel <adysamuel68@gmail.com>
2026-09-06 11:33:52 +0000

    Update websocket test message

--- src/websocket/test_socket.py
+++ src/websocket/test_socket.py
@@ -6,7 +6,7 @@ import asyncio
 
 async def websocket():
     async with websockets.connect("ws://localhost:8000/ws/") as conn:
-        await conn.send("Hey, how are you doinng")
+        await conn.send("Hey, how are you ")
         response = await conn.recv()
         print(response)
         
